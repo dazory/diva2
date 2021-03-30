bec5f700d50b70ea888b7d301945f755ea98509e
YH <lewis45626@gmail.com>
2021-03-30 10:25:59 +0900

    2021.03.29 youngho

--- test/sensor_parsing/GpsParsingThread.h
+++ test/sensor_parsing/GpsParsingThread.h
@@ -30,26 +30,18 @@ class GpsParsingThread{
         
 };
 
-inline static bool s_sendmore (zmq::socket_t & socket, const std::string & string) {
-
-zmq::message_t message(string.size());
-
-memcpy (message.data(), string.data(), string.size());
-
-bool rc = socket.send (message, ZMQ_SNDMORE);
-
-return (rc);
-
+inline static bool 
+s_sendmore(zmq::socket_t & socket, const std::string & string) {
+    zmq::message_t message(string.size());
+    memcpy(message.data(), string.data(), string.size());
+    bool rc = socket.send(message, ZMQ_SNDMORE);
+    return (rc);
 }
 
-inline static bool s_send (zmq::socket_t & socket, const std::string & string, int flags = 0) {
-
-zmq::message_t message(string.size());
-
-memcpy (message.data(), string.data(), string.size());
-
-bool rc = socket.send (message, flags);
-
-return (rc);
-
-}
+inline static bool
+s_send(zmq::socket_t & socket, const std::string & string, int flags = 0) {
+        zmq::message_t message(string.size());
+        memcpy(message.data(), string.data(), string.size());
+        bool rc = socket.send(message, flags);
+        return (rc);
+    }
