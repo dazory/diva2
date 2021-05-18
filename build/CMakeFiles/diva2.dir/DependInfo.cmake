be82c59630b4d493855a8403b3aab52da3f18bc3
kayeon <oooac999@gmail.com>
2021-05-18 16:44:11 +0900

    save can raw data csv file

--- build/CMakeFiles/diva2.dir/DependInfo.cmake
+++ build/CMakeFiles/diva2.dir/DependInfo.cmake
@@ -4,20 +4,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/diva2/diva2/main.cpp" "/home/diva2/diva2/build/CMakeFiles/diva2.dir/main.cpp.o"
+  "/home/kayeon/diva2/main.cpp" "/home/kayeon/diva2/build/CMakeFiles/diva2.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "BOOST_ERROR_CODE_HEADER_ONLY"
-  )
-
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/share/c++-mscl/source"
-  "/usr/share/c++-mscl/Boost/include"
-  "/usr/include/opencv"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
   )
 
 # Targets to which this target links.
