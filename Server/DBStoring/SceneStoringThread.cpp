e0fb4d9e4b57221972b0c0e2b3122e082d4093f6
dazory <dazory@naver.com>
2021-07-04 04:54:24 +0900

    Add Server

---
+++ Server/DBStoring/SceneStoringThread.cpp
@@ -0,0 +1,88 @@
+// 참고: https://github.com/kka-na/DIVA_Qt
+// 참고: https://www.tutorialspoint.com/postgresql/postgresql_c_cpp.htm
+
+#include "SceneStoringThread.h"
+
+using namespace std;
+
+SceneStoringThread::SceneStoringThread(){}
+
+void SceneStoringThread::run(){
+    char * sql;
+    time_t system_time;
+    struct tm* systime;
+    system_time = time(NULL);
+    systime = localtime(&system_time);
+
+    string tm_year = to_string(systime->tm_year + 1900);
+    string tm_month = to_string(systime->tm_mon+1);
+    string tm_date = to_string(systime->tm_mday);
+    if(tm_date.size() == 1){
+        tm_date = "0"+tm_date;
+    }
+
+    string timestamp;
+    timestamp = tm_year + tm_month + tm_date;
+   
+   try {
+      connection C("dbname = diva2db user = diva2 password = 1234 \
+      hostaddr = 127.0.0.1 port = 5432");
+      if (C.is_open()) {
+         cout << "Opened database successfully: " << C.dbname() << endl;
+      } else {
+         cout << "Can't open database" << endl;
+      }
+
+      string path = "/home/cvlab2/DIVA2/diva2/Server/DIVA2_DATA/"+timestamp+"_0/JSON/scene.json";
+
+      /* Create SQL statement */
+      sql = "create table SCENE(first_frame_token text primary key,log_token text, nbr_frames text);";
+      
+      /* Create a transactional object. */
+      work W(C);
+
+      /* Execute SQL query */
+      W.exec( sql );
+      W.commit();
+
+      Json::Value Scenes;
+      ifstream scene_in(path.c_str());
+      if(scene_in.is_open()) scene_in>>Scenes;
+      std::string temp1;
+      std::string temp2;
+      std::string temp3;      
+
+      for(int i=0; i<Scenes.size(); i++){
+         temp1=std::string((Scenes[i]["first_frame_token"].asString()).c_str());
+         temp2=std::string((Scenes[i]["log_token"].asString()).c_str());
+         temp3=std::string((Scenes[i]["nbr_frames"].asString()).c_str());
+
+         /* Create a transactional object. */
+         work W(C);
+	      std::string query_string;
+         query_string.append("insert into SCENE values('");
+
+	      // *****Set table*****
+         query_string.append(temp1);
+         query_string.append("','");
+         query_string.append(temp2);
+         query_string.append("','");
+         query_string.append(temp3);
+         query_string.append("');");
+
+         /* Execute SQL query */
+         W.exec(query_string);
+         W.commit();
+      }
+      
+      cout << "[DBStoring] scene successfully" << endl;
+      C.disconnect ();
+   } catch (const std::exception &e) {
+      cerr << e.what() << std::endl;
+   }
+
+}
+
+void SceneStoringThread::stop(){
+    stop_flag = true;
+}
---
+++ build/Server/DBStoring/CMakeFiles/dbstoring.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/CamStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/CamStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/CanStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/CanStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/FrameStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/FrameStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/GpsStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/GpsStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/ImuStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/ImuStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/LidarStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/LidarStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/LogStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/LogStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/SceneStoringThread.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/SceneStoringThread.cpp.o"
+  "/home/cvlab2/DIVA2/diva2/Server/DBStoring/main.cpp" "/home/cvlab2/DIVA2/diva2/build/Server/DBStoring/CMakeFiles/dbstoring.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ERROR_CODE_HEADER_ONLY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/share/c++-mscl/source"
+  "/usr/share/c++-mscl/Boost/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
