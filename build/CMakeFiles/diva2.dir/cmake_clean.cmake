00f4f529bc5f65d6c7a4c9f36a2dc09606fdad47
ka-yeon <12181736@inha.edu>
2021-07-04 14:13:46 +0900

    restore

---
+++ build/CMakeFiles/diva2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diva2.dir/main.cpp.o"
+  "diva2"
+  "diva2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diva2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Server/TimeSynchronization/CMakeFiles/timesynchronization.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timesynchronization.dir/indexandtimes.cpp.o"
+  "CMakeFiles/timesynchronization.dir/main.cpp.o"
+  "CMakeFiles/timesynchronization.dir/makejson.cpp.o"
+  "CMakeFiles/timesynchronization.dir/makejsonThread.cpp.o"
+  "CMakeFiles/timesynchronization.dir/readfiles.cpp.o"
+  "timesynchronization"
+  "timesynchronization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/timesynchronization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
